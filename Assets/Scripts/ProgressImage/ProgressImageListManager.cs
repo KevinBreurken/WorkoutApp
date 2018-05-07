ba137d2d9300a0f9e9d495db0cea9d69d5218358
KevinBreurken <kevinbreurken@hotmail.com>
2018-05-07 23:12:41 +0200

    Added #3 to ProgressViewerScreen

--- Assets/Scripts/ProgressImage/ProgressImageListManager.cs
+++ Assets/Scripts/ProgressImage/ProgressImageListManager.cs
@@ -6,13 +6,26 @@ public class ProgressImageListManager : MonoBehaviour {
 
     [SerializeField]
     private ProgressImageManager progressImageManager;
-
+    [SerializeField]
+    private ProgressImageListSortButton sortButton;
     [SerializeField]
     private GameObject uiItemPrefab;
 
     private List<ProgressUIItem> spawnedItems = new List<ProgressUIItem>();
     [SerializeField]
     private RectTransform itemHolder;
+
+    private bool sortDescending = true;
+
+    void Awake ()
+    {
+        sortButton.onSortingChanged += SortButton_onSortingChanged;
+    }
+
+    private void SortButton_onSortingChanged (bool _state)
+    {
+        DisplayItems();
+    }
 
     public void DisplayItems ()
     {
@@ -24,11 +37,18 @@ public class ProgressImageListManager : MonoBehaviour {
         }
         spawnedItems.Clear();
 
-        for (int i = 0; i < progressImageManager.GetAllProgressImages().Count; i++)
+        List<ProgressImage> newCarouselProgressImages = progressImageManager.GetAllProgressImages();
+        newCarouselProgressImages.Sort((p1, p2) => p1.dateCompareValue.CompareTo(p2.dateCompareValue));
+        if (sortButton.GetState())
+        {
+            newCarouselProgressImages.Reverse();
+        }
+
+        for (int i = 0; i < newCarouselProgressImages.Count; i++)
         {
             GameObject spawnedObject = Instantiate(uiItemPrefab, itemHolder);
             ProgressUIItem UIItem = spawnedObject.GetComponent<ProgressUIItem>();
-            UIItem.progressImage = progressImageManager.GetAllProgressImages()[i];
+            UIItem.progressImage = newCarouselProgressImages[i];
             UIItem.UpdateUI();
             UIItem.onRemoveClicked += UIItem_onRemoveClicked;
             spawnedItems.Add(UIItem);
---
+++ Assets/Scripts/UI Components/ProgressImageListSortButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressImageListSortButton : MonoBehaviour {
+
+    public delegate void OnSortingClickedEvent (bool _state);
+    public event OnSortingClickedEvent onSortingChanged = delegate
+    { };
+
+    private Button button;
+    private Image image;
+
+    public bool isDescending = true;
+
+    [SerializeField]
+    private Sprite ascendingSprite;
+    [SerializeField]
+    private Sprite descendingSprite;
+
+    void Awake ()
+    {
+        
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnToggleClicked);
+        image = GetComponent<Image>();
+        UpdateGraphic();
+    }
+
+    void OnToggleClicked ()
+    {
+        isDescending = !isDescending;
+        onSortingChanged(isDescending);
+        UpdateGraphic();
+    }
+
+    void UpdateGraphic ()
+    {
+        if (isDescending)
+            image.sprite = ascendingSprite;
+        else
+            image.sprite = descendingSprite;
+
+    }
+
+    public bool GetState ()
+    {
+        return isDescending;
+    }
+}
